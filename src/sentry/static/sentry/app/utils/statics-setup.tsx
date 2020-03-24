c2984221146805f25b414c428ece22063024383d
Burak Yigit Kaya <ben@byk.im>
2020-03-24 22:55:50 +0300

    fix eslint

--- src/sentry/static/sentry/app/utils/statics-setup.tsx
+++ src/sentry/static/sentry/app/utils/statics-setup.tsx
@@ -1,8 +1,7 @@
 /* eslint no-native-reassign:0 */
 
-// This (global) errors for some reason :|
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-let var __webpack_public_path__: string;
+// eslint-disable-next-line no-var
+declare var  __webpack_public_path__:string; 
 
 /**
  * Set the webpack public path at runtime. The __sentryGlobalStaticPrefix will
