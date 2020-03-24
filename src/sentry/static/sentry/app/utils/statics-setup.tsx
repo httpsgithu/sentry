04a774c40779933d8a44899b463a8d2dc6c7eba2
Burak Yigit Kaya <ben@byk.im>
2020-03-24 22:14:05 +0300

    try to fix build

--- src/sentry/static/sentry/app/utils/statics-setup.tsx
+++ src/sentry/static/sentry/app/utils/statics-setup.tsx
@@ -2,7 +2,7 @@
 
 // This (global) errors for some reason :|
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-/* global __webpack_public_path__ */
+declare var  __webpack_public_path__:string; 
 
 /**
  * Set the webpack public path at runtime. The __sentryGlobalStaticPrefix will
